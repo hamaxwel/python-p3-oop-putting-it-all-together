531d668816d251741c9a16879c9e9f71ceab0a77
maxwel kirimi <maxwelkirimii@gmail.com>
2024-11-28 15:57:37 +0300

    assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,53 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
+    def __init__(self, title, page_count, year_published=None, price=None):
+        self.title = title
+        self.page_count = page_count
+        self.year_published = year_published
+        self.price = price
     
-        
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and value:
+            self._title = value
+        else:
+            raise ValueError("Title must be a non-empty string.")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")  
+
+    @property
+    def year_published(self):
+        return self._year_published
+
+    @year_published.setter
+    def year_published(self, value):
+        if value is None or isinstance(value, int):
+            self._year_published = value
+        else:
+            raise ValueError("Year published must be an integer.")
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value is None or isinstance(value, (int, float)):
+            self._price = value
+        else:
+            raise ValueError("Price must be a number.")
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str) and value:
+            self._brand = value
+        else:
+            raise ValueError("Brand must be a non-empty string.")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")  
+
+    def cobble(self):
+        print("Your shoe is as good as new!") 
